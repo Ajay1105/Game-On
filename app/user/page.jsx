4a1d3088b200dbcc2ec7812e080e321b9fbd1146
Ajay <aky11052003@gmail.com>
2024-03-03 16:52:32 +0530

    Add Navbar component and update page layout

--- app/user/page.jsx
+++ app/user/page.jsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import { useUser } from "@clerk/clerk-react";
 import Image from "next/image";
+import Navbar from "@/components/Navbar/Navbar";
 
 const Page = () => {
   const { isSignedIn, user } = useUser();
@@ -30,45 +31,55 @@ const Page = () => {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center">
-      {isSignedIn && (
-        <div className="mb-4">
-          <p className="text-lg font-semibold my-8">Welcome {user.fullName}</p>
-          <div className="mt-2 flex justify-center my-8">
-            <Image
-              src={user.imageUrl}
-              alt="user image"
-              width={100}
-              height={100}
-              priority={true}
-              className="rounded-full"
-            />
+    <>
+      <Navbar />
+      <div className="flex flex-col items-center justify-center">
+        {isSignedIn && (
+          <div className="mb-4">
+            <p className="text-2xl font-semibold my-8">
+              Welcome {user.fullName}
+            </p>
+            {/*<div className="mt-2 flex justify-center my-8">
+              <Image
+                src={user.imageUrl}
+                alt="user image"
+                width={100}
+                height={100}
+                priority={true}
+                className="rounded-full"
+              />
+        </div>*/}
           </div>
-        </div>
-      )}
-      {isLoading ? (
-        <div className="text-gray-600">Loading...</div>
-      ) : transactions && transactions.length > 0 ? (
-        <div className="mt-4">
-          <p className="text-lg font-semibold mb-2">Bookings</p>
-          {transactions.map((transaction) => (
-            <div
-              key={transaction._id}
-              className="bg-gray-100 p-4 rounded-md mb-2"
-            >
-            <div className="text-sm text-gray-600">
-            Time: {new Date(transaction.time).toLocaleString()}
-          </div>
-              <div className="text-sm text-gray-600">
-                Amount: {transaction.amount}
+        )}
+        {isLoading ? (
+          <div className="text-gray-600">Loading...</div>
+        ) : transactions && transactions.length > 0 ? (
+          <div className="mt-4">
+            <p className="text-lg font-semibold mb-2 italic">Bookings</p>
+            {transactions.map((transaction) => (
+              <div
+                key={transaction._id}
+                className="bg-gray-100 p-4 rounded-2xl mb-2 flex flex-row"
+              >
+                <div>
+                  <img src="./icons/booking.png" alt="booking" className="w-10 h-10 mx-5 mt-2" />
+                </div>
+                <div>
+                  <div className="text-sm md:text-lg mr-5 text-gray-600">
+                    Time: {new Date(transaction.time).toLocaleString()}
+                  </div>
+                  <div className="text-sm md:text-base mr-5 text-gray-600">
+                    Amount: {transaction.amount}
+                  </div>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-      ) : (
-        <div className="text-gray-600">No transactions found.</div>
-      )}
-    </div>
+            ))}
+          </div>
+        ) : (
+          <div className="text-gray-600">No transactions found.</div>
+        )}
+      </div>
+    </>
   );
 };
 
--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -28,8 +28,8 @@ const Navbar = () => {
         <div className="menu-container">
             <ul className={`menu ${menuOpen ? 'open' : ''}`}>
             <li className='item'><a href="/user">Bookings</a></li>
-            <li className='item'><a href="/">About Us</a></li>
-            <li className='item'><a href="/">Gallery</a></li>
+            <li className='item'><a href="/about">About Us</a></li>
+            <li className='item'><a href="/gallery">Gallery</a></li>
             <li className='item'><a href="/contact">Contact Us</a></li>
             <li className='user-icon'><UserButton /></li>
             </ul>
