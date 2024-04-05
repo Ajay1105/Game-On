563f640f997670188d9ae2aab12b10301d0ad309
Ajay <aky11052003@gmail.com>
2024-04-05 23:31:57 +0530

    Add transaction creation logic in Razorpay webhook

--- app/api/webhook/razorpay/route.js
+++ app/api/webhook/razorpay/route.js
@@ -1,6 +1,9 @@
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import crypto from "crypto";
+import { createTransaction } from "@/app/actions/transaction.action";
+import { User } from "@clerk/nextjs/dist/types/server";
+import connectDB from "../../mongodb/connectDB";
 
 export async function POST(req, res){
     const secret = process.env.WEBHOOK_RAZORPAY_SECRET;
@@ -16,6 +19,34 @@ export async function POST(req, res){
         return NextResponse.json({status:400});
     }
     console.log("Razorpay Webhook received");
+
+    let foundUser;
+    const email = body.payload.payment.entity?.email || "";
+    try {
+      await connectDB();
+      foundUser = await User.findOne({ email });
+    } catch (error) {
+      return NextResponse.json({ message: "User not found", error: error });
+    }
+
+    try {
+      
+        const transaction = {
+          amount: body.payload.payment.entity.amount/100 ,
+          stadiumId: body.payload.payment.entity.notes?.stadiumId || "",
+          time: body.payload.payment.entity.notesbody.payload.payment.entity.notes?.time || 0,
+          buyerId:  foundUser._id || "",
+          createdAt: new Date(),
+          captainName: body.payload.payment.entity.notes?.captainName || "",
+          phoneNumber: body.payload.payment.entity.notes?.phoneNumber || "",
+          noOfPlayers: body.payload.payment.entity.notes?.noOfPlayers || "",
+        };
+    
+        await createTransaction(transaction);
+        
+      } catch (error) {
+        return NextResponse.json({ message: "error", err: error });  
+      }
 
     return NextResponse.json({status:200});
 }
