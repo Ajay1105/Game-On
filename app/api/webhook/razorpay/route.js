00019fead02caef9db977cde7c2188b156ca79cd
Ajay <aky11052003@gmail.com>
2024-04-05 23:19:04 +0530

    Add signature verification for Razorpay webhook

--- app/api/webhook/razorpay/route.js
+++ app/api/webhook/razorpay/route.js
@@ -6,7 +6,16 @@ export async function POST(req, res){
     const body = await req.json();
     const headersList= headers()
 
-    console.log(headersList.get('x-razorpay-signature'));
+    //console.log(headersList.get('x-razorpay-signature'));
+    const hashedSign = crypto.createHmac('sha256', secret)
+    hashedSign.update(JSON.stringify(body))
+    const generatedSignature = hashedSign.digest('hex')
+    
+    if(headersList.get('x-razorpay-signature') !== generatedSignature){
+        return NextResponse.json({status:400});
+    }
+    console.log("Razorpay Webhook received");
+
     return NextResponse.json({status:200});
 }
 
